0e7d29e17c8f58c815530fb3db0ce238b0525046
saomi <saomi@foxmail.com>
2020-05-02 18:49:23 +0800

    Initial commit

---
+++ lib/pages/loginPage.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _LoginPageState();
+  }
+
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Container(
+        decoration: new BoxDecoration(
+          color: Colors.red,
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: NetworkImage("https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3609617849,3076246071&fm=26&gp=0.jpg")
+          )
+        ),
+        child: Center(
+            heightFactor: 2,
+            child: SizedBox(
+              width: 300,
+              height: 320,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        fillColor: Colors.white,
+                        filled: true,
+                        labelText: '账号',
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        fillColor: Colors.white,
+                        filled: true,
+                        labelText: '密码',
+                      ),
+                    ),
+                  ),
+                  RaisedButton(
+                    child: Text("登录"),
+                    color: Colors.orange,
+                    onPressed: (){
+                      Navigator.of(context).pushNamed("routes/mainPage");
+                    },
+                  )
+                ],
+              ),
+            )
+        ),
+      ),
+    );
+  }
+
+}
---
+++ lib/pages/mainPage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MainPageState();
+  }
+
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+
+    );
+  }
+
+}
