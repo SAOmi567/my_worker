e9427a668fe075c0b84f993ea0170af83ad14921
saomi <saomi@foxmail.com>
2020-05-03 03:09:32 +0800

    在家更新

---
+++ lib/pages/CustomerPage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomerPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _CustomerPageState();
+  }
+
+}
+
+class _CustomerPageState extends State<CustomerPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+
+    );
+  }
+
+}
--- lib/pages/mainPage.dart
+++ lib/pages/mainPage.dart
@@ -11,12 +11,43 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  List<Widget> _modulePageList = <Widget>[
+
+  ];
   @override
   Widget build(BuildContext context) {
+    // 创建悬浮按钮
+    Widget _floatingActionButton = FloatingActionButton(
+        onPressed: () {
+          Scaffold.of(context).openDrawer();
+        },
+        child: Icon(Icons.widgets)
+    );
+    Scaffold s = null;
     // TODO: implement build
     return Scaffold(
       body: Column(
 
+      ),
+      floatingActionButton: _floatingActionButton,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.lightGreen
+              ),
+              child: Text(
+                "侧边栏"
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.contacts),
+              title: Text("客户"),
+            )
+          ],
+        ),
       ),
     );
   }
